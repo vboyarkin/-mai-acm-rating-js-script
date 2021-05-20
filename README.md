8f0fd42265b75e5de43c5dac4c0a43d135a2615d
nyaq <nyaaqq@gmail.com>
2021-05-20 21:17:22 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# -mai-acm-rating-js-script
+# mai-acm-rating-js-script
+
+Этот юзерскрипт считает на страницах рейтинга потенциальные баллы. Предполагается, что одна задача, решённая на контесте, равна 3-м задачам на дорешке. Осенняя/весенняя олимпиады считаются так же и не обрабатываются отдельно.
+
+## Как установить?
+
+1. Tampermonkey:
+    - [Tampermonkey для chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=ru)
+    - [Tampermonkey для firefox](https://addons.mozilla.org/ru/firefox/addon/tampermonkey/)
+
+2. Нажми [сюда](https://github.com/vboyarkin/-mai-acm-rating-js-script/raw/master/userscript.js) и нажми `Установить`
---
+++ usersctipt.js
@@ -0,0 +1,49 @@
+// ==/UserScript==
+
+// ==UserScript==
+// @name mai-acm-rating-js-script 
+// @description 
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       vboyarkin
+// @include mai2020.contest.codeforces.com/*/customrating/
+// ==/UserScript==
+
+(function () {
+    const CONTEST = 3;
+    const PRACTICE = 1;
+
+    let guys_rows = document.querySelector('tbody').children;
+    let guys_r1 = document.querySelector('thead').children[0];
+    let guys_garbage = document.querySelector('thead').children[1];
+
+    let th = document.createElement("th");
+    guys_r1.insertBefore(th, guys_r1.children[1]);
+    guys_garbage.insertBefore(document.createElement("th"), guys_garbage.children[0]);
+    th.innerText = "Примерные баллы"
+
+    let scores = [].map.call(guys_rows, row => {
+        return [].reduce.call(row.children, (acc, cell) => {
+
+            if (cell.classList.contains('overall-custom-rating-practice') && cell.classList.contains('overall-custom-rating-accepted')) return acc + PRACTICE;
+            else if (cell.classList.contains('overall-custom-rating-contestant') && cell.classList.contains('overall-custom-rating-accepted')) return acc + CONTEST;
+
+            return acc;
+        }, 0);
+    })
+
+    let highest = Math.max.apply(null, scores);
+
+    scores = scores.map(score => (score / highest) * 1.5);
+
+    [].forEach.call(guys_rows, (row, i) => {
+        let sc_td = document.createElement("td");
+        sc_td.classList.toggle("left");
+        row.insertBefore(sc_td, row.children[1]);
+        let score = Math.round(scores[i] * 100) / 100;
+        sc_td.innerText = score;
+    })
+
+    return scores
+
+})()
